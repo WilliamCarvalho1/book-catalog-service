d1280c262e1261c45e611f76b2d16d8e60298941
William Carvalho <carvalhowilliam081@gmail.com>
2025-07-05 17:39:05 +0100

    Refactoring

--- src/main/java/com/studies/adapters/entity/BookEntity.java
+++ src/main/java/com/studies/adapters/entity/BookEntity.java
@@ -1,36 +1,27 @@
 package com.studies.adapters.entity;
 
+//import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.lang.annotation.Documented;
 
 @Data
 @Builder
-//@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+//@Entity
 //@Table(name = "books")
-//public class BookEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String id;
-//    private String name;
-//    private String author;
-//    private String category;
-//    private String status;
-//    private double price;
-//}
-
 @Document(collection = "books")
 public class BookEntity {
 
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private String author;
