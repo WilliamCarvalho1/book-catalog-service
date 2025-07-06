ddf9f23673f531bf663187a5137866b946fb866e
William Carvalho <carvalhowilliam081@gmail.com>
2025-07-06 16:42:54 +0100

    removed blank line

--- src/main/java/com/studies/adapters/entity/BookEntity.java
+++ src/main/java/com/studies/adapters/entity/BookEntity.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 //import org.springframework.data.annotation.Id;
 //import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Data
 @Builder
 @NoArgsConstructor
