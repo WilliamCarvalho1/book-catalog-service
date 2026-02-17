83c77c9a962c080d67fb2a2621532d2f35c67838
qxz384g <william.carvalho@ctw.bmwgroup.com>
2026-02-17 10:03:39 +0000

    Removes @NotNull validation from price in BookUpdateRequestDTO

--- src/main/java/com/studies/bookcatalog/adapter/in/controller/dto/BookUpdateRequestDTO.java
+++ src/main/java/com/studies/bookcatalog/adapter/in/controller/dto/BookUpdateRequestDTO.java
@@ -1,13 +1,11 @@
 package com.studies.bookcatalog.adapter.in.controller.dto;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 
 public record BookUpdateRequestDTO(
-        @NotNull(message = "price must not be null")
         @Positive(message = "price must be positive")
         BigDecimal price,
 
