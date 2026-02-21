ea54dc4074854871423784221b8a6304339a3db8
qxz384g <william.carvalho@ctw.bmwgroup.com>
2026-02-21 18:27:04 +0000

    Adds @Getter to CartExportProperties

--- src/main/java/com/studies/bookstore/infrastructure/configuration/CartExportProperties.java
+++ src/main/java/com/studies/bookstore/infrastructure/configuration/CartExportProperties.java
@@ -1,8 +1,10 @@
 package com.studies.bookstore.infrastructure.configuration;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 @ConfigurationProperties(prefix = "cart.export")
 public class CartExportProperties {
@@ -11,10 +13,6 @@ public class CartExportProperties {
      * Base directory where shopping cart export JSON files will be written.
      */
     private String directory;
-
-    public String getDirectory() {
-        return directory;
-    }
 
     public void setDirectory(String directory) {
         this.directory = directory;
